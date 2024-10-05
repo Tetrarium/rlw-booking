7e86ee70061d99dde48fe58bb435a2fea47c12e3
AlexandrTurov <alexandr.turov@mail.ru>
2024-10-05 13:14:24 +0700

    add global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,23 @@
+import "./global.sass";
+
+import { Roboto } from "next/font/google";
+
 import Layout from "../components/layout";
 
 import type { AppProps } from 'next/app';
 
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["100", "300", "900"],
+});
+
 export default function Home({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <div className={roboto.className}>
+
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </div>
   );
 }
