9c3fa5342f0c55a62bd0c2b268dc563dbb7fa9dc
AlexandrTurov <alexandr.turov@mail.ru>
2024-10-21 00:39:45 +0700

    add head to layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import React, { ReactNode } from "react";
 
 import Footer from "./footer/footer";
@@ -6,6 +7,9 @@ import Header from "./header/header";
 export default function Layout({ children }: { children: ReactNode; }) {
   return (
     <>
+      <Head>
+        <title>Сервис продажи билетов</title>
+      </Head>
       <Header />
       <main>{children}</main>
       <Footer />
