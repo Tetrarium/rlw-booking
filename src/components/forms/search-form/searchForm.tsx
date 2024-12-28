fbdbe177a2773a7a077bd3a983bb6dab9ebaa099
AlexandrTurov <alexandr.turov@mail.ru>
2024-12-28 12:25:57 +0700

    provide data to search form

--- src/components/forms/search-form/searchForm.tsx
+++ src/components/forms/search-form/searchForm.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
+import { useAppDispatch, useAppSelector } from "@/lib/hooks";
+import {
+    changeDepartureCity, changeDestinationCity, reverseLocations
+} from "@/lib/reducers/locationsSlice";
 import FindButton from "@/UI/buttons/findButton";
 import Calendar from "@/UI/calendar/calendar";
 import { Cached } from "@mui/icons-material";
@@ -9,6 +13,9 @@ import CitySearchField from "./CitySearchField/CitySearchField";
 import s from "./searchForm.module.sass";
 
 const SearchForm = () => {
+  const { departure, destination } = useAppSelector(state => state.locations);
+  const dispatch = useAppDispatch();
+
   return (
     <form className={s.search}>
       <div className={s.row}>
@@ -18,21 +25,23 @@ const SearchForm = () => {
         <div className={s.fields}>
           <div className={s.field}>
             <CitySearchField
+              key={departure._id}
               label="Откуда"
-              city={{ _id: '', name: '' }}
-              onSelect={() => { }}
+              city={departure}
+              onSelect={(_id, name) => dispatch(changeDepartureCity({ _id, name }))}
             />
           </div>
           <div className={s.btn__place}>
-            <IconButton color="inherit">
+            <IconButton color="inherit" onClick={() => dispatch(reverseLocations())}>
               <Cached />
             </IconButton>
           </div>
           <div className={s.field}>
             <CitySearchField
+              key={destination._id}
               label="Куда"
-              city={{ _id: '', name: '' }}
-              onSelect={() => { }}
+              city={destination}
+              onSelect={(_id, name) => dispatch(changeDestinationCity({ _id, name }))}
             />
           </div>
         </div>
