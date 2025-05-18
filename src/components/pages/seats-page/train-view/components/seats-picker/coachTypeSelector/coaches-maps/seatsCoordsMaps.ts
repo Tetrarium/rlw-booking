1efcad7d5b03e18f511c8c24918282419cf944ab
AlexandrTurov <alexandr.turov@mail.ru>
2025-05-18 15:02:47 +0700

    feat(coachMap): add function getFirstSeatsCoords

--- src/components/pages/seats-page/train-view/components/seats-picker/coachTypeSelector/coaches-maps/seatsCoordsMaps.ts
+++ src/components/pages/seats-page/train-view/components/seats-picker/coachTypeSelector/coaches-maps/seatsCoordsMaps.ts
@@ -6,6 +6,37 @@ export interface SeatCoords {
   strokeWidth: number;
 }
 
+function getFirstSeatsCoords(): SeatCoords[] {
+  const TOTAL_SEATS = 16;
+  const INNER_STEP_X = 60;
+  const OUTER_STEP_X = 30;
+
+  const WIDTH = 23;
+  const HEIGHT = 56;
+  const STROKE_WIDTH = 2;
+  const START_X = 136;
+
+  const seats: SeatCoords[] = [];
+  let currentX = START_X;
+
+  for (let i = 1; i <= TOTAL_SEATS; i++) {
+    seats.push({
+      x: currentX,
+      y: 30,
+      width:
+        WIDTH,
+      height: HEIGHT,
+      strokeWidth: STROKE_WIDTH,
+    });
+
+    currentX += i % 2 === 0 ? OUTER_STEP_X : INNER_STEP_X;
+  }
+
+  return seats;
+}
+
+export const firstSeatsCoords = getFirstSeatsCoords();
+
 function getSecondSeatsCoords(): SeatCoords[] {
   const TOTAL_SEATS = 32;
   const INNER_STEP_X = 60;
